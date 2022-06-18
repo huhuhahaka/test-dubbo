13cffaa10b5d8ee0dbd6da2919b7f72e84dedd54
郭光林 <guoguanglin@mooe-robot.com>
2022-06-18 20:51:13 +0800

    整合 Jacoco

--- consumer/src/test/java/com/example/consumer/ConsumerApplicationTests.java
+++ consumer/src/test/java/com/example/consumer/ConsumerApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.consumer;
 
+import com.example.consumer.controller.UserController;
+import javax.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ConsumerApplicationTests {
 
+    @Resource
+    private UserController userController;
+
     @Test
     void contextLoads() {
+        userController.getUser("小明", null);
+        userController.hello();
     }
 
 }
